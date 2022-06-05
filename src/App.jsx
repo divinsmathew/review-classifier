b0d8eefc589b00aef2d9878a705241bc5124bb3b
Divins Mathew <divinsmathew@gmail.com>
2022-06-05 19:31:35 +0530

    Updated for interview v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,67 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
+const Sentiment = ({ sentiment }) => {
+    return (
+        <div className="sentiment">
+            {sentiment === "positive" ? (
+                <span className="sentiment-icon positive">Positive</span>
+            ) : (
+                <span className="sentiment-icon negative">Negative</span>
+            )}
+        </div>
+    );
+};
+
 const App = () => {
-    const filePickerRef = useRef();
-    const [selectedFileName, setSelectedFileName] =
-        useState("No file selected");
     const [isUploading, setIsUploading] = useState(false);
     const [classifiedData, setClassifiedData] = useState([]);
+    const [review, setReview] = useState("");
+    const [inputMode, setInputMode] = useState(true);
 
-    const fileSelected = async (event) => {
-        setSelectedFileName(event.target.files[0].name);
-        setIsUploading(true);
-        await uploadFile(event.target.files[0]);
-        filePickerRef.current.value = null;
-    };
+    // const sleep = (ms) => {
+    //     return new Promise((resolve) => setTimeout(resolve, ms));
+    // };
 
-    const uploadFile = async (file) => {
+    const sendRequest = async (file) => {
         const fileData = new FormData();
         fileData.append("file", file);
         setClassifiedData([]);
         setIsUploading(true);
-        const response = await fetch(
-            "https://flask-review-app.herokuapp.com/predict",
-            {
-                method: "POST",
-                body: fileData,
-            }
-        );
+
+        // await sleep(1000);
+        const response = await fetch("<URL GOES HERE>", {
+            method: "POST",
+            body: fileData,
+        });
         const data = await response.json();
+
+        // const data = {
+        //     results: {
+        //         sentence_wise_sentiment: [
+        //             {
+        //                 sentence: "Hi I am good",
+        //                 sentiment: "positive",
+        //             },
+        //             {
+        //                 sentence: "This is bad",
+        //                 sentiment: "negative",
+        //             },
+        //         ],
+        //         sentiment: "positive",
+        //         title: "It is good",
+        //     },
+        // };
+
         setClassifiedData(data.results);
         setIsUploading(false);
 
-        if (data.results.length === 0) alert("No results found");
+        if (data?.results?.length === 0) {
+            alert("No results found");
+            return;
+        }
+
+        setInputMode(false);
     };
 
     return (
@@ -41,70 +71,123 @@ const App = () => {
                 <span>=</span>
             </header>
             <main>
-                <div className="input-container">
-                    <div>Upload your CSV dataset here.</div>
-                    <div className="warning-text">
-                        Please make sure that reviews in file are free of
-                        gibberish and emoticons.
-                    </div>
-                    <input
-                        ref={filePickerRef}
-                        type="file"
-                        id="file-picker"
-                        accept=".csv"
-                        onChange={fileSelected}
-                    />
-                    <button
-                        className="upload-button"
-                        onClick={() => filePickerRef.current.click()}
-                        disabled={isUploading}
-                    >
-                        {isUploading ? "Uploading..." : "Upload"}
-                    </button>
-                    <div className="file-selected-label">
-                        {selectedFileName}
-                    </div>
-                    {classifiedData.length > 0 && (
-                        <article>
-                            <div className="title">
-                                Found {classifiedData.length} Results:
-                            </div>
-                            <div className="classified-data-container">
-                                <table className="table table-bordered table-hover table-condensed">
-                                    <thead>
-                                        <tr>
-                                            <th>Review</th>
-                                            <th>Stars</th>
-                                            <th>URL</th>
-                                            <th>User Name</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {classifiedData.map((data, index) => (
-                                            <tr
-                                                className="classified-data"
-                                                key={index}
-                                            >
-                                                <td>{data.review}</td>
-                                                <td>{data.star}</td>
-                                                <td>
-                                                    <a
-                                                        href={data.url}
-                                                        target="_blank"
-                                                        rel="noreferrer"
-                                                    >
-                                                        Click to open
-                                                    </a>
-                                                </td>
-                                                <td>{data.user_name}</td>
+                {inputMode && (
+                    <div className="input-container">
+                        <div>Enter your review here.</div>
+                        <div className="warning-text">
+                            Please make sure that reviews in file are free of
+                            gibberish and emoticons.
+                        </div>
+                        <textarea
+                            value={review}
+                            placeholder="Enter your review here."
+                            onChange={(event) => setReview(event.target.value)}
+                        />
+                        <button
+                            className="upload-button"
+                            onClick={sendRequest}
+                            disabled={isUploading}
+                        >
+                            {isUploading ? "Submitting..." : "Submit"}
+                        </button>
+                        {classifiedData.length > 0 && (
+                            <article>
+                                <div className="title">
+                                    Found {classifiedData.length} Results:
+                                </div>
+                                <div className="classified-data-container">
+                                    <table className="table table-bordered table-hover table-condensed">
+                                        <thead>
+                                            <tr>
+                                                <th>Review</th>
+                                                <th>Stars</th>
+                                                <th>URL</th>
+                                                <th>User Name</th>
                                             </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                            {classifiedData.map(
+                                                (data, index) => (
+                                                    <tr
+                                                        className="classified-data"
+                                                        key={index}
+                                                    >
+                                                        <td>{data.review}</td>
+                                                        <td>{data.star}</td>
+                                                        <td>
+                                                            <a
+                                                                href={data.url}
+                                                                target="_blank"
+                                                                rel="noreferrer"
+                                                            >
+                                                                Click to open
+                                                            </a>
+                                                        </td>
+                                                        <td>
+                                                            {data.user_name}
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            )}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </article>
+                        )}
+                    </div>
+                )}
+                {!inputMode && (
+                    <div className="input-container">
+                        <h4>Summary</h4>
+
+                        <div className="summary-container">
+                            <div className="summary-text">
+                                <span className="summary-title">Title</span>
+                                <span>{classifiedData.title}</span>
                             </div>
-                        </article>
-                    )}
-                </div>
+                            <div className="summary-text">
+                                <span className="summary-title">Sentiment</span>
+                                <Sentiment
+                                    sentiment={classifiedData.sentiment}
+                                />
+                            </div>
+                        </div>
+                        <h4>Sentence-wise sentiments</h4>
+                        <table className="table table-bordered table-hover table-condensed">
+                            <thead>
+                                <tr>
+                                    <th>Sentence</th>
+                                    <th>Sentiment</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {classifiedData.sentence_wise_sentiment.map(
+                                    (data, index) => (
+                                        <tr
+                                            className="classified-data"
+                                            key={index}
+                                        >
+                                            <td>{data.sentence}</td>
+                                            <td>
+                                                <Sentiment
+                                                    sentiment={data.sentiment}
+                                                />
+                                            </td>
+                                        </tr>
+                                    )
+                                )}
+                            </tbody>
+                        </table>
+
+                        <button
+                            className="upload-button"
+                            onClick={() => setInputMode(true)}
+                            disabled={isUploading}
+                        >
+                            Submit New Review
+                        </button>
+                    </div>
+                )}
             </main>
         </div>
     );
